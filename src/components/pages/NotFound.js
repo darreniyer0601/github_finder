37d2a74bc67fcf955121c9df476af8546206c79e
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2020-11-05 07:14:06 -0500

    First Commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NotFound = () => {
+    return (
+        <div>
+            <h1>404 Error: Not Found</h1>
+            <p className="lead">The page you are looking for does not exist.</p>
+        </div>
+    )
+}
+
+export default NotFound;
